26761418cce1e40ba45a4e1c057e555a8fd2f53f
Artyom Bauer <artem_bauer@inbox.ru>
2025-03-08 22:35:34 +0700

    feat: added migrations

--- UndergroundBank/UndergroundBank.LoanService.Infrastructure/Migrations/20250308153514_AddedTransactionDB.Designer.cs
+++ UndergroundBank/UndergroundBank.LoanService.Infrastructure/Migrations/20250308153514_AddedTransactionDB.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UndergroundBank.LoanService.Infrastructure;
@@ -11,9 +12,11 @@ using UndergroundBank.LoanService.Infrastructure;
 namespace UndergroundBank.LoanService.Infrastructure.Migrations
 {
     [DbContext(typeof(LoanDbContext))]
-    partial class LoanDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250308153514_AddedTransactionDB")]
+    partial class AddedTransactionDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ UndergroundBank/UndergroundBank.LoanService.Infrastructure/Migrations/20250308153514_AddedTransactionDB.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UndergroundBank.LoanService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTransactionDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "InterestRate",
+                table: "Tariffs",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    AccountNumber = table.Column<string>(type: "text", nullable: false),
+                    StartAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "InterestRate",
+                table: "Tariffs",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+    }
+}
