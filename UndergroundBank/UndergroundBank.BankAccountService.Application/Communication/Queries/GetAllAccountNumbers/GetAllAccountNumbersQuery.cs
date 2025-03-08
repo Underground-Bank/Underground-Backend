71b7c08788c892b37c41371e746ed0fd39bd4a01
Ruslan Maksyutov <rusmaksiutov@gmail.com>
2025-03-08 20:42:08 +0700

    feat: add get account numbers for correspoding user

--- UndergroundBank/UndergroundBank.BankAccountService.Application/Communication/Queries/GetAllAccountNumbers/GetAllAccountNumbersQuery.cs
+++ UndergroundBank/UndergroundBank.BankAccountService.Application/Communication/Queries/GetAllAccountNumbers/GetAllAccountNumbersQuery.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MediatR;
+﻿using MediatR;
 using UndergroundBank.Common.Dto.BankAccountService;
 
 namespace UndergroundBank.BankAccountService.Application.Communication.Queries.GetAllAccountNumbers
